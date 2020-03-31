ac377b3ff86d9d2d851ad5c21259d390f7a524c2
Kishore S <kishore@Kishores-MacBook-Pro.local>
2020-03-31 21:06:56 +0530

    Updates

---
+++ precache-manifest.36466a53cd070d1d3f6b49241e343710.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d4c864ef9b6eff1895e0ffdf56e65d",
+    "url": "/chennai-bank/index.html"
+  },
+  {
+    "revision": "129d788a72a814e198b2",
+    "url": "/chennai-bank/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "df8132a87c71bad0cb31",
+    "url": "/chennai-bank/static/css/main.3764f26b.chunk.css"
+  },
+  {
+    "revision": "129d788a72a814e198b2",
+    "url": "/chennai-bank/static/js/2.b49dcbc3.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/chennai-bank/static/js/2.b49dcbc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df8132a87c71bad0cb31",
+    "url": "/chennai-bank/static/js/main.48c80a2a.chunk.js"
+  },
+  {
+    "revision": "c7a8f8e50ceb26dd1dfd",
+    "url": "/chennai-bank/static/js/runtime-main.24b90889.js"
+  },
+  {
+    "revision": "c993dbe12a541cd7c193084d39c5d2d0",
+    "url": "/chennai-bank/static/media/chip.c993dbe1.png"
+  },
+  {
+    "revision": "4e8ac07a83e569215531abb9446eddf3",
+    "url": "/chennai-bank/static/media/ssl.4e8ac07a.png"
+  }
+]);
